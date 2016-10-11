1016f7a85b1010b9d7669139c7e25e1a91be774b
Jackson Shafeiev <jackson_shafeiev@outlook.com>
2016-10-11 18:43:03 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,172 @@
+/*var result = 0;
+function add(number){
+    var result = 0;
+    result += number;
+}
+add(5);
+add(15);
+console.log(result);*/
+
+/*function greet(who, text) {
+    who = ', ' + who + '!';
+    alert(text + who);
+}
+
+var who = "John";
+
+greet(who, "Hello");
+
+alert(who);*/
+
+/*function checkAge(age){
+  return  (age < 18) ? false  :  " you are allowed";
+}
+var res = checkAge(10);
+alert(res);*/
+
+/*function min(a,b,c){
+    
+    if (a < b && a < c ){
+        return a;
+    }else if 
+        (b < a && b < c){
+        return b;
+    }
+        return c;
+
+    
+}
+alert(min(17,2,3));*/
+
+/*function min(a,b,c){
+    return a < b && a < c && a ||
+           b < a && b < c && b ||
+           c;
+}
+
+
+alert(min(5,55,8))*/
+
+/*function pow(a, b){
+    if (b === 0){
+        return 1;
+    }
+    if(b<0){
+        var negative = true;
+        b = -b;
+    }
+    
+    var result = a;
+    
+    for (var i = 1; i < b; i++){
+        result *= a;
+    }
+    return negative? 1/result : result;
+}
+
+
+alert(pow(2, 3));
+alert(pow(2, -4));
+alert(pow(2, -3));*/
+
+
+
+
+/*function greet(greeteng, name){
+    if(arguments.length < 2){
+        return "warning"
+    }
+    greeteng = greeteng || "Hi";
+    name = name || 'Stranger';
+    
+    return greeteng + ', ' + name;
+}
+
+
+var res1 = greet('Hello','John');
+var res2 = greet(null, "John");
+var res3 = greet('Hello', null);
+var res4 = greet('Hello');
+
+
+console.log(res1);
+console.log(res2);
+console.log(res3);
+console.log(res4);*/
+
+
+/*var arr = [1, 2, 3];
+ function my(arr){
+     for(var i = 1; i < arguments.length; i++){
+     arr.push(arguments[i]);
+    }
+ }
+my(arr, 'one', 'ttt', 'fgfdf');
+console.log(arr);
+*/
+
+/*var arr = [1, 5, 9, 8, 77, 45, 89, 25, 46];
+
+
+function my(arr){
+    var result = {
+        odd: [],
+        even: []
+    }
+    for(var i = 0; i < arguments.length; i++){
+        if(arr[i]  % 2) ?
+            result.odd.push(arr[i]):
+            result.even.push(arr[i])
+}
+    return result;
+    }
+console.log(my(array));
+______________________________________________________________________________*/
+
+
+/*function Unseen(a){
+   return (a < 2) ?
+       0 :
+        Math.pow(a - 2,3);
+}
+
+var result1 = Unseen(2);
+var result2 = Unseen(3);
+var result3 = Unseen(4);
+var result4 = Unseen(5);
+
+
+console.log(result1, result2, result3, result4);*/
+
+
+
+/*var arr1 = [1,2,3];
+var arr2 = [6,7,8,];
+var str = `the result of division `;
+var res = str.split(' ');
+
+
+console.log(res);*/
+
+
+function str(a){
+    return str.split()
+}
+str(i wanna rock);
+console.log(str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
